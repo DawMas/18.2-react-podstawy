6b61f8c95c75dfb9b44d5946daec7a519ce9a4b9
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-07-04 19:31:23 +0200

    Repair visibility pictures

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ var movies = [
         id: 1,
         title: 'Harry Potter',
         desc: 'film o czarodzieju',
-        poster: './images/hary.jpg'
+        src: './images/hary.jpg'
     },
     {
         id: 2,
         title: 'Król lew',
         desc: 'Film o królu sawanny',
-        poster: 'https://ssl-gfx.filmweb.pl/po/63/16/776316/7875036.2.jpg'
+        src: 'https://ssl-gfx.filmweb.pl/po/63/16/776316/7875036.2.jpg'
     },
     {
         id: 3,
         title: 'Władca pierścieni',
         desc: 'film o hobbicie i pierścionku',
-        poster: 'https://ssl-gfx.filmweb.pl/po/10/65/1065/6900087.2.jpg'
+        src: 'https://ssl-gfx.filmweb.pl/po/10/65/1065/6900087.2.jpg'
     },
     {
         id: 4,
         title: 'Taxi',
         desc: 'film o szybkiej taksówce',
-        poster: 'https://ssl-gfx.filmweb.pl/po/07/27/727/6900934.2.jpg'
+        src: 'https://ssl-gfx.filmweb.pl/po/07/27/727/6900934.2.jpg'
     },
 ];
 
@@ -29,7 +29,7 @@ var moviesElements = movies.map(function (movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc),
-        React.createElement('img', {src: movies.poster, alt: movies.title}),
+        React.createElement('img', {src: movie.src, alt: movie.title}),
         
     );
 });
